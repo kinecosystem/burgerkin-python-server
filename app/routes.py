b8cb8fac1d1be449cdb612459575aa196fbeca00
Shahar Solomianik <shaharsol@Shahars-MacBook-Pro.local>
2019-08-04 15:20:07 +0300

    contd

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
 from app import app
+from kin import KinClient, TEST_ENVIRONMENT
+import asyncio
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/login')
+async def login():
+    async with KinClient(TEST_ENVIRONMENT) as client:
+        exists = await client.does_account_exists("dfdfdsfdsfsd")
+    if exists:
+        return "exists"
+    else:
+        return "doesnt exist"
